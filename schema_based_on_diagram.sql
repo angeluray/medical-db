b4d6d506f88f9a93195b692c7441423bcb9d513d
Mov305 <mov3three2001@gmail.com>
2022-10-25 19:06:07 +0200

    added the shema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,51 +1,51 @@
 CREATE TABLE patients (
     id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     name VARCHAR(255) NOT NULL,
-    date_of_birth DATE NOT NULL
-    PRIMARY KEY(id);
+    date_of_birth DATE NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE medical_histories (
-    id INTEGER NOT NULL,
+    id INTEGER GENERATED AlWAYS AS IDENTITY,
     admitted_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     patient_id INTEGER NOT NULL,
-    status VARCHAR(255) NOT NULL
-    PRIMARY KEY(id);
+    status VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE treatments (
-    id INTEGER NOT NULL,
+    id INTEGER GENERATED AlWAYS AS IDENTITY,
     type VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL
-    PRIMARY KEY(id);
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE invoice_items (
-    id INTEGER NOT NULL,
+    id INTEGER GENERATED AlWAYS AS IDENTITY,
     unit_price DECIMAL(8, 2) NOT NULL,
     quantity INTEGER NOT NULL,
     total_price DECIMAL(8, 2) NOT NULL,
     invoice_id INTEGER NOT NULL,
-    treatment_id INTEGER NOT NULL
-    PRIMARY KEY(id);
-    CONSTRAINT invoice_items_treatment_id_foreign FOREIGN KEY(treatment_id) REFERENCES treatments(id);
+    treatment_id INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT invoice_items_treatment_id_foreign FOREIGN KEY(treatment_id) REFERENCES treatments(id)
 );
 
 CREATE TABLE invoices (
-    id INTEGER NOT NULL,
+    id INTEGER GENERATED AlWAYS AS IDENTITY,
     total_amount DECIMAL(8, 2) NOT NULL,
     generated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     payed_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    medical_history_id INTEGER NOT NULL
-    PRIMARY KEY(id);
-    CONSTRAINT invoices_medical_history_id_foreign FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id);
+    medical_history_id INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT invoices_medical_history_id_foreign FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 );
 
 CREATE TABLE histories_treatments(
-    id INTEGER NOT NULL,
+    id INTEGER GENERATED AlWAYS AS IDENTITY,
     history_id INTEGER NOT NULL,
-    treatment_id INTEGER NOT NULL
-    PRIMARY KEY(id);
+    treatment_id INTEGER NOT NULL,
+    PRIMARY KEY(id)
 );
 
 ALTER TABLE histories_treatments ADD CONSTRAINT histories_treatments_history_id_foreign FOREIGN KEY(history_id) REFERENCES medical_histories(id);
